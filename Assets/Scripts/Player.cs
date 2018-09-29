998cc572695d0e67d4286cf9d31a9f50f741e791
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2018-09-29 15:24:59 +0100

    Movements added to player

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+	[SerializeField] float distanceFromCenter = 1f;
+	void move () {
+		var deltaX = Input.GetAxis("Horizontal");
+		var newPosX = Mathf.Clamp(
+			transform.position.x + deltaX,
+			-distanceFromCenter,
+			distanceFromCenter
+		);
+		Vector2 newPos = new Vector2(newPosX, transform.position.y);
+		transform.position = newPos;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		move();
+	}
+}

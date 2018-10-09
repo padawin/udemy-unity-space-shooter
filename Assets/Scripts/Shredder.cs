bedcce66da103b2059b82fde903beb4931e1fb96
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2018-10-09 21:51:53 +0100

    Missiles destroyed when leaving the screen

---
+++ Assets/Scripts/Shredder.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shredder : MonoBehaviour {
+	// Update is called once per frame
+	void OnTriggerEnter2D (Collider2D collider) {
+		Destroy(collider.gameObject);
+	}
+}

5673ae024e45d2fdcb22054c3ecee75a9f1ac4c1
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2018-10-19 06:52:28 +0100

    Enemies can fire now

---
+++ Assets/Scripts/EnemyFireContinuous.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyFireContinuous : MonoBehaviour {
+	Gun gun;
+
+	// Use this for initialization
+	void Start () {
+		gun = GetComponentInChildren<Gun>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (!gun.isFiring()) {
+			gun.fire();
+		}
+	}
+}

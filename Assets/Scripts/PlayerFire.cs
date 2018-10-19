4a1403a619472e6f9547cd2656dba2c3eb1f0846
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2018-10-19 18:21:56 +0100

    Player script renamed

--- Assets/Scripts/PlayerFire.cs
+++ Assets/Scripts/PlayerFire.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : MonoBehaviour {
+public class PlayerFire : MonoBehaviour {
 	Gun gun;
 
 	void Start() {

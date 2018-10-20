86dd1fb3822f2041fd27df97215c89b9d1152ef3
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2018-10-20 18:06:03 +0100

    Single shot weapon added to have some random not regular shots

--- Assets/Scripts/Actor/Enemy/EnemyFireContinuous.cs
+++ Assets/Scripts/Actor/Enemy/EnemyFireContinuous.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class EnemyFireContinuous : MonoBehaviour {
 	Gun gun;
+	float timeToNextShot = 0;
+	[SerializeField] float minTimeBetweenShots = 0.2f;
+	[SerializeField] float maxTimeBetweenShots = 3f;
+	
 
 	// Use this for initialization
 	void Start () {
@@ -12,6 +16,10 @@ public class EnemyFireContinuous : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		gun.fire();
+		timeToNextShot -= Time.deltaTime;
+		if (timeToNextShot <= 0f) {
+			gun.fire();
+			timeToNextShot = Random.Range(minTimeBetweenShots, maxTimeBetweenShots);
+		}
 	}
 }
---
+++ Assets/Scripts/Weapons/SingleShot.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SingleShot : Gun {
+	public override void fire() {
+		_fire();
+	}
+
+	public override void stopFire() {
+	}
+}
